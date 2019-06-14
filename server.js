b18ddd68424a53b23f0d0f6f91421059a30d4fcd
benjaminmballa <mballabenjamin@yahoo.fr>
2019-06-14 10:39:33 +0200

    Clean

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors);
 
@@ -9,6 +11,6 @@ app.get("/", (req, res) => {
   res.send("HELLO WORLD");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server run on localhost:${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server run on localhost:${port}`);
 });
