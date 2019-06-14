8c84518a9a48bfe44002f60f00db440b145024ba
benjaminmballa <mballabenjamin@yahoo.fr>
2019-06-14 10:48:05 +0200

    Clean

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
-app.use(cors);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("HELLO WORLD");
